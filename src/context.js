45c5285149b9d0cbb8fea2a9693db3c145a2698f
fatimanazeer <factoombatool@gmail.com>
2023-05-28 00:13:10 -0700

    Initial commit

---
+++ src/context.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModelOpen] = useState(false)
+
+  const openSidebar = () => {
+      setIsSidebarOpen(true)
+    }
+  const closeSidebar = () => {
+        setIsSidebarOpen(false)
+      }
+  const openModal = () => {
+        setIsModelOpen(true)
+      }
+  const closeModal = () => {
+        setIsModelOpen(false)
+      }
+  return <AppContext.Provider
+   value={{
+    isModalOpen,
+    isSidebarOpen,
+    openModal,
+    openSidebar,
+    closeModal,
+    closeSidebar
+   }}>
+   {children}
+  </AppContext.Provider>
+}
+// COUSTOM HOOK
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+export {AppContext, AppProvider}
